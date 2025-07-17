651753c1e9d145b4205d6621d34b9e8eea0709a3
ArthurDenis62 <113990502+ArthurDenis62@users.noreply.github.com>
2025-07-17 14:09:52 +0200

    Bills correction

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -7,14 +7,20 @@ import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
+import store from "../app/Store.js";
+
+jest.mock("../app/Store");
+
+beforeEach(() => {
+  Object.defineProperty(window, "localStorage", { value: localStorageMock });
+  window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+});
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-      Object.defineProperty(window, "localStorage", { value: localStorageMock });
-      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
@@ -47,10 +53,59 @@ describe("Given I am connected as an employee", () => {
       const $ = require("jquery");
       global.$ = $;
       $.fn.modal = jest.fn();
-      const BillsContainer = require("../containers/Bills.js").default;
-      new BillsContainer({ document, onNavigate: jest.fn(), store: null, localStorage });
+      new Bills({ document, onNavigate: jest.fn(), store: null, localStorage });
       icon.click();
       expect($.fn.modal).toHaveBeenCalledWith("show");
+    });
+
+    test("fetches bills from mock API GET", async () => {
+      store.bills.mockImplementation(() => ({ list: () => Promise.resolve(bills) }));
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByText("Mes notes de frais"));
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+      expect(screen.getByTestId("tbody").children.length).toBeGreaterThan(0);
+    });
+
+    test("Then getBills() should return formatted bills and catch errors", async () => {
+      store.bills.mockImplementation(() => ({ list: () => Promise.resolve(bills) }));
+      const billInstance = new Bills({ document, onNavigate: jest.fn(), store, localStorage });
+      const data = await billInstance.getBills();
+      expect(data[0]).toHaveProperty("status");
+      expect(Array.isArray(data)).toBe(true);
+    });
+
+    describe("When an error occurs on API", () => {
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        store.bills.mockImplementationOnce(() => ({
+          list: () => Promise.reject(new Error("Erreur 404"))
+        }));
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.append(root);
+        router();
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      });
+
+      test("fetches bills from an API and fails with 500 message error", async () => {
+        store.bills.mockImplementationOnce(() => ({
+          list: () => Promise.reject(new Error("Erreur 500"))
+        }));
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.append(root);
+        router();
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/);
+        expect(message).toBeTruthy();
+      });
     });
   });
 });
